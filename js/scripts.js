93868360bdaf2862ffc062e2b973407e5f44b773
hannah melendy and Lindsey Baker <lindsey@>
2019-03-27 16:50:29 -0700

    add functional converters (3)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,43 @@
-var conversion = function() {
-  var gallon = parseInt (prompt("enter gallons"));
-  return ((gallon * 3.785) + "liters");
+var gal_litre = function(number) {
+  parseInt(number);
+  return ((number* 3.785) + " liters");
 };
 
-alert(conversion());
+var cups_tbsp = function(number) {
+  parseInt(number);
+  return ((number * 16) + "tbsp");
+};
+
+var pints_cups = function(number) {
+  parseInt(number);
+  return ((number * 2) + "cup");
+};
+
+//User Interface
+
+$(document).ready(function() {
+  $("form#gal_litre").submit(function(event) {
+    event.preventDefault();
+    var gallonInput = $("#gallon").val();
+    var result = gal_litre(gallonInput);
+    $("#outputlitre").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#cups_tbsp").submit(function(event) {
+    event.preventDefault();
+    var cupInput = $("#cup").val();
+    var result = cups_tbsp(cupInput);
+    $("#outputtbsp").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#pints_cups").submit(function(event) {
+    event.preventDefault();
+    var pintInput = $("#pint").val();
+    var result = pints_cups(pintInput);
+    $("#outputcup").text(result);
+  });
+});
